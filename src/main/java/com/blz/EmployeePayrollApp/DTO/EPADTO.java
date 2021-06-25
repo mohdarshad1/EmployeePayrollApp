916fad3bf6d1ff250c83cda78ab3f048643e37b9
mohdarshad1 <mdarshad181@gmail.com>
2021-06-25 20:09:25 +0530

    [Mohd Arshad] : Ensure validations is done on the Payroll DTO

--- src/main/java/com/blz/EmployeePayrollApp/DTO/EPADTO.java
+++ src/main/java/com/blz/EmployeePayrollApp/DTO/EPADTO.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -21,15 +24,21 @@ public class EPADTO {
 	@Min(value = 5000, message = "Min wage should be more than 5000 ")
 	public long salary;
 
+	@Pattern(regexp = "male|female", message = "Gender needs to be male or female")
 	public String gender;
 
 	@JsonFormat(pattern = "dd MMM yyyy")
+	@NotNull(message = "start date should not be Empty")
+	@PastOrPresent(message = "startDate should be past or present date")
 	public LocalDate startDate;
 
+	@NotBlank(message = "Note cannot be Empty")
 	public String note;
 
+	@NotBlank(message = "ProfilePic cannot be Empty")
 	public String profilePic;
 
+	@NotNull(message = "Department should not be Empty")
 	public List<String> departments;
 
 }
